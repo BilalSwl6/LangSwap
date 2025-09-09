b5c4d414d40cb4aca563bde5378ebe3ffca9fd2a
mbilal2913 <mbilal2913@gmail.com>
2025-09-09 22:09:44 +0000

    fix eslint issues

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,3 +1,1 @@
-export class SignupDto {
-  //
-}
+export class SignupDto {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -75,12 +75,6 @@ export class UsersService {
     });
   }
 
-  // update(id: number, updateUserDto: UpdateUserDto) {
-  //   const user = this.repo.update(id, updateUserDto);
-  //   console.log(user);
-  //   return `This action updates a #${id} user`;
-  // }
-
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
